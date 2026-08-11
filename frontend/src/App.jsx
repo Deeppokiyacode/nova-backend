aa7a606a8879d9e0c9e908e6ec8fb7636ef5affe
Deeppokiyacode <deeprpokiya@gmail.com>
2026-08-11 17:38:19 +0530

    Renamed files to bypass Git cache completely

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Chat from './pages/Chat';
+import Login from './pages/LoginScreen'; // <-- Yahan naam change kiya
+import Chat from './pages/ChatScreen';   // <-- Yahan naam change kiya
 
 export default function App() {
   return (
     <Router>
       <Routes>
-       
         <Route path="/" element={<Login />} />
-       
         <Route path="/chat" element={<Chat />} />
       </Routes>
     </Router>
